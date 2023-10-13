72fbf5f8cf10a496d22e62341d5664d8978b873c
Ben Havis <ben.havis1@gmail.com>
2023-10-13 15:00:01 -0400

    Update ResultsMap.js

--- src/pages/ResultsMap.js
+++ src/pages/ResultsMap.js
@@ -4,18 +4,17 @@ import mapboxgl from 'mapbox-gl';
 mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { MAPBOX_TOKEN } from '../config';
-import localforage from 'localforage'; // Import LocalForage
 
 const ResultsMap = ({ results }) => {
   const [map, setMap] = useState(null);
-  const [center, setCenter] = useState([0, 0]);
+  const [center, setCenter] = useState([0, 0]); // Default center at [0, 0]
 
   useEffect(() => {
-    mapboxgl.accessToken = MAPBOX_TOKEN;
+    mapboxgl.accessToken =  MAPBOX_TOKEN; // Replace with your Mapbox access token
 
     const mapInstance = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: 'mapbox://styles/mapbox/streets-v11', // Use any default Mapbox style that supports OpenStreetMap data
       center: [0, 0],
       zoom: 4,
     });
@@ -23,25 +22,18 @@ const ResultsMap = ({ results }) => {
     setMap(mapInstance);
 
     return () => {
+      // Clean up the map instance when the component unmounts
       mapInstance.remove();
     };
   }, []);
 
   useEffect(() => {
+    console.log("Map Changes")
     const fetchCoordinates = async () => {
       const coordinatesPromises = results.map(async (result) => {
-        console.log('result address:', result.address)
         const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
           result.address
-        )}.json?access_token=${MAPBOX_TOKEN}`;
-
-        // Check if coordinates are already stored in LocalForage
-        const storedCoordinates = await localforage.getItem(result.address);
-
-        if (storedCoordinates) {
-          // If coordinates are found in LocalForage, use them
-          return { ...result, ...storedCoordinates };
-        }
+        )}.json?access_token=${MAPBOX_TOKEN}`; // Replace with your Mapbox access token
 
         try {
           const response = await fetch(geocodeUrl);
@@ -49,10 +41,6 @@ const ResultsMap = ({ results }) => {
 
           if (data.features.length > 0) {
             const [lng, lat] = data.features[0].center;
-
-            // Store the coordinates in LocalForage for future use
-            await localforage.setItem(result.address, { lat, lng });
-
             return { ...result, lat, lng };
           }
         } catch (error) {
@@ -63,6 +51,7 @@ const ResultsMap = ({ results }) => {
       });
 
       const coordinatesData = await Promise.all(coordinatesPromises);
+      
       const filteredCoordinates = coordinatesData.filter(
         (coordinates) => coordinates !== null
       );
@@ -78,30 +67,35 @@ const ResultsMap = ({ results }) => {
         ) / filteredCoordinates.length;
         setCenter([avgLng, avgLat]);
 
+        // Calculate the bounds of marker coordinates
         const bounds = new mapboxgl.LngLatBounds();
         filteredCoordinates.forEach((coord) => {
           bounds.extend([coord.lng, coord.lat]);
         });
 
+        // Fit the map to the bounds with an immediate transition
         if (map) {
           map.fitBounds(bounds, {
-            padding: 20,
-            duration: 0,
+            padding: 20, // Optional padding around the bounds
+            duration: 0, // Set the duration to 0 for an immediate transition
           });
           console.log('Map bounds set:', bounds);
         }
       }
 
       if (map) {
-        map.on('load', () => {
+        // map.on('load', () => {
           console.log('Map Loaded');
+          console.log("Filtered", filteredCoordinates)
           filteredCoordinates.forEach((coordinates) => {
             const marker = new mapboxgl.Marker()
               .setLngLat([coordinates.lng, coordinates.lat])
               .addTo(map);
             console.log('Marker added:', marker);
+            
+            // Optionally add marker click event handling here
           });
-        });
+        // });
       }
     };
 
