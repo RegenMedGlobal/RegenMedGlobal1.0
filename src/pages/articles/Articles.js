b1542ef6270e8a59ab352ce2cefe4a4b09b272ab
sumitsehgal <theprofessional1992@gmail.com>
2023-11-04 12:03:09 +0530

    Fixed Footer Issue on Article List Page

--- src/pages/articles/Articles.js
+++ src/pages/articles/Articles.js
@@ -21,8 +21,9 @@ const StyledArticleContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  height: 100vh;
+  min-height: 80vh;
   margin-top:8%;
+  margin-bottom:20px;
 
   @media (max-width: 865px) {
     /* Adjust top margin for screens narrower than 768px (mobile) */
