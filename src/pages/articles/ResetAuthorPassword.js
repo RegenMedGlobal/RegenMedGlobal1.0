b3d270ffec42689e9f1b015117a7805dc3d7f76f
Ben Havis <ben.havis1@gmail.com>
2023-11-11 16:39:58 -0500

    add in reset pass

--- src/pages/articles/ResetAuthorPassword.js
+++ src/pages/articles/ResetAuthorPassword.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import { Form, Input, Button, message } from 'antd';
+import React, { useState } from 'react';
+import { Form, Input, Button, message, Typography } from 'antd';
 import styled from 'styled-components';
+import { createClient } from '@supabase/supabase-js';
+import resetAuthorPassword from './functions/resetAuthorPassword';
+import zxcvbn from "zxcvbn";
+
+const { Text } = Typography;
 
 const StyledContainer = styled.div`
   max-width: 400px;
@@ -31,40 +36,132 @@ const StyledContainer = styled.div`
   button {
     align-self: flex-end;
   }
+
+   .success-message {
+    font-size: 18px;
+    font-weight: bold;
+    color: #4811ab;
+    margin-top: 16px;
+  }
 `;
+
 const ResetAuthorPassword = () => {
   const [form] = Form.useForm();
+  const [showPasswordFields, setShowPasswordFields] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isChecking, setIsChecking] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+   const [successMessage, setSuccessMessage] = useState(null);
 
-  const onFinish = async (values) => {
+
+  const handleCheck = async () => {
     try {
-      const { email } = values;
-      const otp = Math.floor(100000 + Math.random() * 900000).toString();
+      const values = await form.validateFields(['email', 'authorName']);
+      console.log('values', values)
+      const { email, authorName } = values;
 
-      // Call the Netlify serverless function to send the email
-      const response = await fetch('/.netlify/functions/sendAuthorEmail', {
-        method: 'POST',
-        body: JSON.stringify({ email, otp }),
-      });
+     const SUPABASE_URL = 'https://sxjdyfdpdhepsgzhzhak.supabase.co';
+const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4amR5ZmRwZGhlcHNnemh6aGFrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4ODc1MDE2NiwiZXhwIjoyMDA0MzI2MTY2fQ.2_rrSgYe0ncUmBlRZAKiHN_q22RsqqNXsjamTRVujz8';
 
-      if (response.ok) {
-        message.success('Password reset email sent. Check your inbox.');
-        // Redirect or handle OTP verification as needed
-      } else {
-        throw new Error('Error sending reset email.');
-      }
-    } catch (error) {
-      console.error('Error sending reset email:', error.message);
-      message.error('Error sending reset email. Please try again.');
+
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+    // Check if the email and author name match the data in the Supabase author_data table
+    const { data, error } = await supabase
+      .from('author_data')
+      .select()
+      .eq('email', email)
+      .eq('authorName', authorName);
+
+    console.log('Supabase Response:', { data, error });
+
+    if (error) {
+      throw error;
     }
-  };
 
-  const validateConfirmEmail = (_, value) => {
-    const { email } = form.getFieldsValue(['email']);
-    if (value !== email) {
-      return Promise.reject('The two emails do not match!');
+    const authorDataMatches = data && data.length > 0;
+
+    if (authorDataMatches) {
+      setShowPasswordFields(true);
+        setErrorMessage(null);
+      // Perform additional actions if needed
+    } else {
+      setErrorMessage('Invalid author name or email.')
+      console.error('Invalid author name or email.');
+      // Display an error message or handle it in your preferred way
     }
-    return Promise.resolve();
-  };
+    console.log('End of handleCheck');
+  } catch (error) {
+    console.error('Error:', error);
+    // Display an error message or handle it in your preferred way
+  } finally {
+    setIsChecking(false);
+  }
+};
+
+const handleCancel = () => {
+  setShowPasswordFields(false);
+  setErrorMessage(null);
+  form.resetFields(); // Reset form fields
+};
+
+const handleResetPassword = async () => {
+  try {
+    setIsSubmitting(true);
+    console.log('Starting handleResetPassword');
+
+    // Get form values directly without waiting for validation
+    const values = form.getFieldsValue(['email', 'authorName', 'password', 'confirmPassword']);
+    console.log('values in reset pass:', values);
+    const { email, authorName, password, confirmPassword } = values;
+
+    if (password !== confirmPassword) {
+      setErrorMessage('Passwords do not match.');
+      return;
+    }
+
+    // Check password strength using zxcvbn
+    const passwordStrength = zxcvbn(password);
+    console.log('Password strength:', passwordStrength);
+
+    if (passwordStrength.score < 2) {
+      // You can customize the error message based on the strength
+      setErrorMessage('Please choose a stronger password.');
+      return;
+    }
+
+    console.log('Resetting password...'); // Log for debugging
+
+    // Reset the author's password using the resetAuthorPassword function
+    await resetAuthorPassword(email, password);
+
+    console.log('Password reset successfully'); // Log for debugging
+
+    message.success('Password reset successfully');
+  
+    // Display success message
+      setSuccessMessage('Password reset successfully');
+      setErrorMessage(null);
+
+      // Hide password fields
+      setShowPasswordFields(false);
+
+    // Clear form fields
+    form.resetFields();
+  } catch (error) {
+    console.error('Error during password reset validation:', error);
+    setErrorMessage('Error resetting password. Please try again.');
+  
+  } finally {
+    setIsSubmitting(false);
+    console.log('After finally block');
+  }
+};
+
+
+
+
+console.log('End of component rendering');
 
   return (
     <StyledContainer>
@@ -72,10 +169,19 @@ const ResetAuthorPassword = () => {
 
       <Form
         form={form}
-        onFinish={onFinish}
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
       >
+        <Form.Item
+          label="Author Name"
+          name="authorName"
+          rules={[
+            { required: true, message: 'Please input your author name!' },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
         <Form.Item
           label="Email"
           name="email"
@@ -87,21 +193,61 @@ const ResetAuthorPassword = () => {
           <Input type="email" />
         </Form.Item>
 
-        <Form.Item
-          label="Confirm Email"
-          name="confirmEmail"
-          rules={[
-            { required: true, message: 'Please confirm your email!' },
-            { validator: validateConfirmEmail },
-          ]}
-        >
-          <Input type="email" />
-        </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Send Reset Email
-          </Button>
+
+        {showPasswordFields && (
+          <>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: 'Please input your password!' },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+              <Form.Item
+              label="Confirm Password"
+              name="confirmPassword"
+              dependencies={['password']}
+              rules={[
+                { required: true, message: 'Please confirm your password!' },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return null; // No error if passwords match
+                    }
+                    return 'The two passwords do not match!';
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+          </>
+        )}
+
+        {errorMessage && (
+          <div className="error-message">{errorMessage}</div>
+        )}
+
+           {successMessage && (
+        <Text className="success-message">{successMessage}</Text>
+      )}
+
+           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          {!showPasswordFields ? (
+            <Button type="primary" onClick={handleCheck} loading={isChecking}>
+              Submit
+            </Button>
+          ) : (
+            <><Button type="primary" onClick={handleResetPassword} >
+                Reset Password
+              </Button><Button onClick={handleCancel} style={{ marginLeft: 8 }}>
+                  Cancel
+                </Button></>
+          )}
         </Form.Item>
       </Form>
     </StyledContainer>
