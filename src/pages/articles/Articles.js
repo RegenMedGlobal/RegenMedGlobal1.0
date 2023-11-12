bf1f18e1f398265617fb616cf717cca66f71ae93
Ben Havis <ben.havis1@gmail.com>
2023-11-11 22:36:59 -0500

    update styling

--- src/pages/articles/Articles.js
+++ src/pages/articles/Articles.js
@@ -28,8 +28,8 @@ const StyledArticleContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  height: 100vh;
-  margin-top:8%;
+  height: 80%;
+  margin-top: 8rem;
 
   @media (max-width: 865px) {
     /* Adjust top margin for screens narrower than 768px (mobile) */
