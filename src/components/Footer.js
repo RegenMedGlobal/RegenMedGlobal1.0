9954ad80fc5646219166f4b2fb467c5643686228
Shugmi Shumunov <SShumunov@gmail.com>
2023-11-14 04:33:12 -0500

    chore: remove additional `;`

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import imgLogo from "../assets/logo.png"
-import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';;
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 import { Link } from "react-router-dom";
 
 const Footer = () => {
