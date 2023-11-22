8f2410c5fac83a833e7f04e8dd8d26a75162b400
Ben Havis <ben.havis1@gmail.com>
2023-11-22 12:53:44 -0500

    update imports

--- src/pages/articles/Article.js
+++ src/pages/articles/Article.js
@@ -1,6 +1,7 @@
 import  { useEffect, useState, useContext } from "react";
 import {useParams, Link } from "react-router-dom";
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_API_KEY, SUPABASE_URL } from "../../config";
 import styled from "styled-components";
 import ReactHtmlParser from "react-html-parser";
 import { AuthContext } from "../../AuthContext";
@@ -124,10 +125,6 @@ const Article = () => {
   const linkTo = articleAuthorId.startsWith('A')
     ? `/author/${articleAuthorId}`
     : `/profile/${articleAuthorId}`;
-
-   const SUPABASE_URL = 'https://sxjdyfdpdhepsgzhzhak.supabase.co';
-  // Should ideally be in an environment variable that's imported
-   const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4amR5ZmRwZGhlcHNnemh6aGFrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4ODc1MDE2NiwiZXhwIjoyMDA0MzI2MTY2fQ.2_rrSgYe0ncUmBlRZAKiHN_q22RsqqNXsjamTRVujz8';
 
   const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
--- src/pages/articles/functions/fetchArticles.js
+++ src/pages/articles/functions/fetchArticles.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_API_KEY, SUPABASE_URL } from '../../../config';
 
-const SUPABASE_URL = 'https://sxjdyfdpdhepsgzhzhak.supabase.co';
-const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4amR5ZmRwZGhlcHNnemh6aGFrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4ODc1MDE2NiwiZXhwIjoyMDA0MzI2MTY2fQ.2_rrSgYe0ncUmBlRZAKiHN_q22RsqqNXsjamTRVujz8';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
