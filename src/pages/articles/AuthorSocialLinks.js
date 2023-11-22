f998e1d6fcc91ccbfcd831e29e5c7bab3b9c1a3b
Ben Havis <ben.havis1@gmail.com>
2023-11-21 21:27:41 -0500

    open social links in new tab

--- src/pages/articles/AuthorSocialLinks.js
+++ src/pages/articles/AuthorSocialLinks.js
@@ -7,22 +7,22 @@ const AuthorSocialLinks = ({ socialLinks }) => {
   return (
     <div>
       {socialLinks.facebookLink && (
-        <a href={socialLinks.facebookLink}>
+        <a href={socialLinks.facebookLink} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faFacebook} />
         </a>
       )}
       {socialLinks.twitterLink && (
-        <a href={socialLinks.twitterLink}>
+        <a href={socialLinks.twitterLink} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faTwitter} />
         </a>
       )}
       {socialLinks.instagramLink && (
-        <a href={socialLinks.instagramLink}>
+        <a href={socialLinks.instagramLink} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faInstagram} />
         </a>
       )}
       {socialLinks.youtubeLink && (
-        <a href={socialLinks.youtubeLink}>
+        <a href={socialLinks.youtubeLink} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faYoutube} />
         </a>
       )}
