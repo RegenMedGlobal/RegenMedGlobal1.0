a523e8482578c079d9dfc158eb88212ee80232ac
Shugmi Shumunov <SShumunov@gmail.com>
2023-11-30 19:25:02 -0500

    chore: Add types for `react-dom` and move import

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ReactGA from 'react-ga';
+import { BrowserRouter } from 'react-router-dom';
 
 // Initialize Google Analytics with your Measurement ID
 ReactGA.initialize('G-E1K3NF9839');
 
-import { BrowserRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-      <App />
-	</BrowserRouter>
-   
+    <App />
+  </BrowserRouter>
 );
-
