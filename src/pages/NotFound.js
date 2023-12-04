ae42e46a01f16477a55ea41e316fa9bb8e37f1ee
Shugmi Shumunov <SShumunov@gmail.com>
2023-12-04 00:08:34 -0500

    refactor: Add link back to homepage on 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
     <div style={{ marginTop: '150px' }}>
       <h1>404: Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
       <p>Check to see if you entered the URL correctly, if you believe you're seeing this in error, contact the website admin</p>
+      <Link to="/">Go back to homepage</Link>
     </div>
   );
 }
