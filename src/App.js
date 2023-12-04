236dce92988b2312440491b9a393e2850f8a37ef
Shugmi Shumunov <SShumunov@gmail.com>
2023-12-03 23:41:02 -0500

    feat: implement lazy loading to improve performance

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Main from './pages/Main'
+import React, { lazy, Suspense, useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Services from './pages/Services'
-import Contact from './pages/Contact'
-import Results from './pages/Results'
-import Profile from './pages/Profile'
-import DoctorLogin from './pages/DoctorLogin'
-import Register from './pages/Register'
-import Logout from './pages/Logout'
-import Claim from './pages/Claim'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import AuthProvider from './AuthContext'
-import ResetPassword from './pages/ResetPassword'
-import Footer from './components/Footer'
 import "@fontsource/poppins";
-import CodeValidator from './pages/CodeValidator'
-import Articles from './pages/articles/Articles'
-import Article from './pages/articles/Article'
-import SubmitArticle from './pages/articles/SubmitArticle'
-import Author from './pages/articles/Author'
-import AuthorSignUp from './pages/articles/AuthorSignUp'
-import AuthorSignIn from './pages/articles/AuthorSignIn'
-import ResetAuthorPassword from './pages/articles/ResetAuthorPassword'
+
+import AuthProvider from './AuthContext'
+import Navbar from './components/Navbar'
+// import Footer from './components/Footer'
+
 import ReactGA from 'react-ga';
-import Videos from './pages/podcasts/Videos'
+
+const Article = lazy(() => import('./pages/articles/Article'));
+const Articles = lazy(() => import('./pages/articles/Articles'));
+const Author = lazy(() => import('./pages/articles/Author'));
+const AuthorSignIn = lazy(() => import('./pages/articles/AuthorSignIn'));
+const AuthorSignUp = lazy(() => import('./pages/articles/AuthorSignUp'));
+const Claim = lazy(() => import('./pages/Claim'));
+const CodeValidator = lazy(() => import('./pages/CodeValidator'));
+const Contact = lazy(() => import('./pages/Contact'));
+const DoctorLogin = lazy(() => import('./pages/DoctorLogin'));
+const Logout = lazy(() => import('./pages/Logout'));
+const Main = lazy(() => import('./pages/Main'));
+const Profile = lazy(() => import('./pages/Profile'));
+const Register = lazy(() => import('./pages/Register'));
+const ResetAuthorPassword = lazy(() => import('./pages/articles/ResetAuthorPassword'));
+const ResetPassword = lazy(() => import('./pages/ResetPassword'));
+const Results = lazy(() => import('./pages/Results'));
+const Services = lazy(() => import('./pages/Services'));
+const SubmitArticle = lazy(() => import('./pages/articles/SubmitArticle'));
+const Videos = lazy(() => import('./pages/podcasts/Videos'));
 
 // Initialize Google Analytics with your Measurement ID
 ReactGA.initialize('G-E1K3NF9839');
@@ -76,28 +80,30 @@ function App() {
       <div className={`App ${className}`}>
         <Navbar />
         <div className="in-body">
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/Services' element={<Services />} />
-          <Route path='/home' element={<Main />} />
-          <Route path='/Contact' element={<Contact />} />
-          <Route path='/Results' element={<Results />} />
-          <Route path='/DoctorLogin' element={<DoctorLogin />} />
-          <Route path='/Logout' element={<Logout />} />
-          <Route path='/Register' element={<Register />} />
-          <Route path='/profile/:userId' element={<Profile />} />
-          <Route path='/claim' element={<Claim />} />
-          <Route path='/articles' element={<Articles />} />
-          <Route path="/article/:articleId" element={<Article />} />
-          <Route path="/author/:authorId" element={<Author />} />
-          <Route path='/submitarticle' element={<SubmitArticle />} />
-          <Route path='/authorsignup' element={<AuthorSignUp />} />
-          <Route path='/authorsignin' element={<AuthorSignIn />} />
-          <Route path='/ResetPassword' element={<ResetPassword />} />
-          <Route path='/resetauthorpassword' element={<ResetAuthorPassword />} />
-          <Route path='/videos' element={<Videos />} />
-          <Route path='/CodeValidator/:id' element={<CodeValidator />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/' element={<Main />} />
+            <Route path='/Services' element={<Services />} />
+            <Route path='/home' element={<Main />} />
+            <Route path='/Contact' element={<Contact />} />
+            <Route path='/Results' element={<Results />} />
+            <Route path='/DoctorLogin' element={<DoctorLogin />} />
+            <Route path='/Logout' element={<Logout />} />
+            <Route path='/Register' element={<Register />} />
+            <Route path='/profile/:userId' element={<Profile />} />
+            <Route path='/claim' element={<Claim />} />
+            <Route path='/articles' element={<Articles />} />
+            <Route path="/article/:articleId" element={<Article />} />
+            <Route path="/author/:authorId" element={<Author />} />
+            <Route path='/submitarticle' element={<SubmitArticle />} />
+            <Route path='/authorsignup' element={<AuthorSignUp />} />
+            <Route path='/authorsignin' element={<AuthorSignIn />} />
+            <Route path='/ResetPassword' element={<ResetPassword />} />
+            <Route path='/resetauthorpassword' element={<ResetAuthorPassword />} />
+            <Route path='/videos' element={<Videos />} />
+            <Route path='/CodeValidator/:id' element={<CodeValidator />} />
+          </Routes>
+        </Suspense>
         </div>
       </div>
     </AuthProvider>
